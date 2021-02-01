5c42fa2feb2469aaae43137d89a78e487d441655
Pasavit Meekhunsut <72618162+pasavit@users.noreply.github.com>
2021-02-01 17:31:29 -0600

    Update APIKeys.cs

--- PlayDate-App/PlayDate-App/APIKeys.cs
+++ PlayDate-App/PlayDate-App/APIKeys.cs
@@ -7,7 +7,7 @@ namespace PlayDate_App
 {
     public static class APIKeys
     {
-        public static string GoogleApi = "AIzaSyAPiapFOG0Qzbm4ehPQ2oiHVWLrNJONepc";
+        public static string GoogleApi = "";
         public static string ServerEmailAddress = "Sproutholmies@gmail.com";
         public static string ServerEmailPassword = "Butterbuns12!";
     }
